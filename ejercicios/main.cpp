20a8f01876056988b395649e8c75580c6c561d06
santisecco <santisecco@gmail.com>
2022-05-29 15:47:51 -0300

    Agrego un array y un for de prueba

--- ejercicios/main.cpp
+++ ejercicios/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
 int main() {
+    int diego[10] = {1,2,3,4,5,6,7,8,9,10};
+    for(auto i : diego){
+        std::cout << i << std::endl;
+    }
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
